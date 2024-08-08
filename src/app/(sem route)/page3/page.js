05fa3d601bdaf01ce909cd07a21c9dd94f2faf5f
F3lPe <felipegonsalves168@gmail.com>
2024-08-08 13:24:47 -0300

    Initial commit from Create Next App

---
+++ src/app/(sem route)/page3/page.js
@@ -0,0 +1,7 @@
+export default function page3(){
+    return(
+        <h1>
+            Page 3
+        </h1>
+    )
+}
---
+++ src/app/page2/page.js
@@ -0,0 +1,8 @@
+export default function page2(){
+
+    return(
+        <h1 style={{display: 'flex', justifyContent:'space-around'}}>
+            Page
+        </h1>
+    )
+}
